87bc68f083ee4bff32c3f272d77b960aa4522769
879372858@qq.com <879372858@qq.com>
2021-03-12 17:52:42 +0800

    更改悬浮锚点和分组

--- src/components/previewFormContainer.vue
+++ src/components/previewFormContainer.vue
@@ -1,29 +1,37 @@
 /**
 * Created by tangyue on 20/11/9
+* props: showAnchor 是否显示悬浮目录组件。但同时，必须有分组且有分组的名称，才能生成悬浮锚点
 */
 <template>
-      <previewForm
-              ref="form"
-              :view="view"
-              :id="id"
-              :rules="rules"
-              :useCustormRule="useCustormRule"
-              :formModel="formModel"
-              :fdFormItems="fdFormItems"
-              :fdFormData="fdFormData"
-      >
-      </previewForm>
+  <div>
+    <previewForm
+            ref="form"
+            :view="view"
+            :id="id"
+            :rules="rules"
+            :useCustormRule="useCustormRule"
+            :formModel="formModel"
+            :fdFormItems="fdFormItems"
+            :fdFormData="fdFormData"
+    >
+    </previewForm>
+
+    <anchor v-if="showAnchor" ref="anchor"></anchor>
+  </div>
+
 </template>
 
 <script>
   import {getUrlQueryParams, showQueryErrorMessage} from '../assets/js/utils'
   import previewForm from './previewForm.vue'
+  import anchor from './anchor/index.vue'
   import {generateElementuiCode} from '../generate_code/generateCode'
 
   export default {
     name: 'previewFormContainer',
     components: {
-      previewForm
+      previewForm,
+      anchor
     },
     props: {
       // 是否为预览模式，模式是编辑模式啦
@@ -65,7 +73,22 @@
       useCustormRule: {
         type: Boolean,
         default: false
-      }
+      },
+      // 是否显示一个右侧的锚点目录
+      showAnchor: {
+        type: Boolean,
+        default: false
+      },
+      // 用于当本组件嵌入其他系统时，其他系统可能有固定显示的头尾部分，这种情况下，可视区域应该是body的高度还要减去头尾的尺寸
+      // 是锚点目录需要用到的功能
+      outerHeaderHeight: {
+        type: Number,
+        default: 0
+      },
+      outerheightHeight: {
+        type: Number,
+        default: 0
+      },
     },
     data () {
       return {
@@ -75,6 +98,14 @@
     watch: {
       view (n, o) {
         console.log('container watch view', this.view)
+      },
+      fdFormItems(n, o) {
+        // 检测到表单项有变后，通知锚点组件重新生成锚点
+        this.$nextTick(_=>{
+          if(this.showAnchor){
+            this.$refs.anchor.reset();
+          }
+        });
       }
     },
     created () {
@@ -83,6 +114,12 @@
       console.log(searchParamObj);
     },
     mounted () {
+      const that = this;
+      if(this.showAnchor){
+        document.body.addEventListener('mousewheel', function(){
+          that.scrollEvent();
+        })
+      }
     },
     methods: {
       // 这些方法是给外部使用的一些便利方法
@@ -92,7 +129,38 @@
       },
       getFormItemsIns () {
         return this.$refs.form.$refs.fdFormItem;
-      }
+      },
+      scrollEvent(e){
+        let outerHeaderHeight = this.outerHeaderHeight || 0;
+        let outerheightHeight = this.outerheightHeight || 0;
+        this.$nextTick(_=>{
+          console.log('sss', this)
+          let scrollItems = this.$el.querySelectorAll('.fd-form-group');
+          console.log('sss', scrollItems)
+
+          // debugger;
+          // let abc = this.$el;
+          // for (let i = scrollItems.length - 1; i >= 0; i--) {
+          //   // 判断滚动条滚动距离是否大于当前滚动项可滚动距离
+          //   let judge = abc.scrollTop >= scrollItems[i].offsetTop - scrollItems[0].offsetTop
+          //   if (judge) {
+          //     debugger;
+          //     this.$refs.anchor.catalogCur = i;
+          //     break;
+          //   }
+          // }
+          for(let i = 0,len = scrollItems.length;i<len;i++){
+            let child = scrollItems[i];
+            let rec = child.getBoundingClientRect();
+            if((rec.y || rec.top) >= outerHeaderHeight
+              &&(rec.y || rec.top) <= (document.body.clientHeight - outerheightHeight))
+            {
+              this.$refs.anchor.catalogCur = i;
+              break;
+            }
+          }
+        })
+      },
     },
 
     generateElementuiCode: generateElementuiCode
